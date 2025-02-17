d491f418b2c8749eaa764911d0f70b498d3a5e1e
farschad <hoshiar@stonks.ltd>
2025-02-17 19:07:58 +0100

    dark mode optimization

--- src/components/theme-provider.tsx
+++ src/components/theme-provider.tsx
@@ -5,5 +5,17 @@ import { ThemeProvider as NextThemesProvider } from "next-themes"
 import { type ThemeProviderProps } from "next-themes/dist/types"
 
 export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
-  return <NextThemesProvider {...props}>{children}</NextThemesProvider>
+  return (
+    <NextThemesProvider
+      attribute="class"
+      defaultTheme="system"
+      enableSystem={true}
+      enableColorScheme={true}
+      storageKey="vitamin-d-theme"
+      disableTransitionOnChange={true}
+      forcedTheme={props.forcedTheme}
+    >
+      {children}
+    </NextThemesProvider>
+  )
 } 
